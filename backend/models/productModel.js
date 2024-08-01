6448d50ccbd7006dd4aba671f4d579d32546ff19
Arpit Das <arpit2001.ad@gmail.com>
2024-08-01 20:41:49 +0530

    updated with review feature

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -68,6 +68,16 @@ const productSchema = mongoose.Schema({
 				type: String,
 				required: true,
 			},
+			avatar: {
+				public_id: {
+					type: String,
+					required: true,
+				},
+				url: {
+					type: String,
+					required: true,
+				},
+			},
 		},
 	],
 	artist: {
